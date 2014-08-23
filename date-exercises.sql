a02ca88e11a0a0226fdb81b7c017bc5b12baf262
john <jhancock1975@gmail.com>
2014-08-23 08:51:13 -0400

    commit

--- date-exercises.sql
+++ date-exercises.sql
@@ -1,11 +1,11 @@
 create database SuppliersAndParts;
 use SuppliersAndParts;
-create table S (snum varchar(255) not null, sname varchar(255), status int, city varchar(255), primary key(snum));
+create table S (snum varchar(255) not null, sname varchar(255),  status int,  city varchar(255), primary key(snum));
 create table P (pnum varchar(255) not null, pname varchar(255), color varchar(255), weight int, city varchar(255), primary key(pnum));
 create table J (jnum varchar(255) not null, jname varchar(255), city varchar(255), primary key(jnum));
 create table SPJ (snum varchar(255), pnum varchar(255), jnum varchar(255), qty int, 
 	primary key(snum,pnum,jnum), foreign key(snum) references S, foreign key(pnum) references P, foreign key(jnum) references J);
-
+create table SP (snum varchar(255), pnum varchar(255), qty int, primary key (snum,pnum), foreign key(snum) references S, foreign key(pnum) references P);
 insert into S values
 ('S1',  'Smith',  '20',  'London' ),
 ('S2',  'Jones',  '10',  'Paris' ),
@@ -58,3 +58,17 @@ insert into SPJ values
 
 
 create view GOOD_SUPPLIERS as select snum, status, city from S where status > 15;
+
+insert into SP values
+('S1', 'P1', '300'),
+('S1', 'P2', '200'),
+('S1', 'P3', '400'),
+('S1', 'P4', '200'),
+('S1', 'P5', '100'),
+('S1', 'P6', '100'),
+('S2', 'P1', '300'),
+('S2', 'P2', '400'),
+('S3', 'P2', '200'),
+('S4', 'P2', '200'),
+('S4', 'P4', '300'),
+('S4', 'P5', '400');
