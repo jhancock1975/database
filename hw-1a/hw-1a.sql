8132bf8885f24ba85b6bc7d6601f554ff34d8ee4
john <jhancock1975@gmail.com>
2014-09-05 20:24:48 -0400

    adding hw-1a answers

---
+++ hw-1a/hw-1a.sql
@@ -0,0 +1,49 @@
+command echo on
+/* Problem 1 (a) */
+select s# from spj,p,j
+where p.weight > 15 
+and j.city='London'
+and spj.j#=j.j#
+and spj.p#=p.p#;
+
+/* Problem 1 (b) */
+
+select s# from spj
+where 
+j# in (select j# from j where j.city='London')
+and
+p# in (select p# from p where p.weight > 15);
+
+/* problem 2 */
+select s# from spj,p
+where p.weight > 15 
+and spj.p#=p.p#
+intersect
+select s# from spj,j
+where j.city='London'
+and spj.j#=j.j#;
+
+/* problem 3 */
+select s# from spj,p
+where p.weight > 15 
+and spj.p#=p.p#
+minus
+select s# from spj,j
+where j.city='London'
+and spj.j#=j.j#;
+
+/* problem 4 */
+select spjx.j#, spjy.j#
+from s, spj spjx, spj spjy
+where s.city='London'
+and spjx.j# < spjy.j#
+and spjx.s#=s.s#
+and spjy.s#=s.s#;
+
+/* problem 5 */
+select city from p where color='Red'
+union
+select city from s where status > 20
+union
+select city from j where jname='Terminal'
+order by city;
