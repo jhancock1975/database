d4918ee941b600aa3586e226736158abb71e3b17
john <jhancock1975@gmail.com>
2014-09-13 16:11:46 -0400

    added word doc and cleaned up script

--- hw-1b/hw1-b.sql
+++ hw-1b/hw1-b.sql
@@ -5,48 +5,25 @@ set echo on
 select p#,avg(qty) from spj 
 group by p# 
 having avg(qty) > 
-( select avg(qty) from spj, p where color = 'Red' and spj.p#=p.p# );
+(select avg(qty) from spj where p# in (select p# from p where p.color='Red'));
 
 /* Problem 7  */
 
 select p# from p where 
 (select avg(qty) from spj where spj.p#=p.p#) >
-(select avg(qty) from spj, p where p.color='Red' and spj.p#=p.p#);
+(select avg(qty) from spj where p# in (select p# from p where color='Red'));
+
 
 /* Problem 8  (a) */
 
-select spj.s# from spj, p, j
-where p.color='Red' and j.city='Rome'
-and spj.j#=j.j# and spj.p#=p.p#
-group by s#
-having count( distinct spj.j#) =
-(select count(distinct j#) from j where j.city='Rome');
-
-/* this is like the one in the lecture */
 select s# from spj where
   j# in (select j# from j where j.city='Rome') and
   p# in (select p# from p where p.color='Red')
 group by s#
 having count(distinct j#)=(select count(*) from j where j.city='Rome');
 
-select * from spj where j#='J2' and p# in ('P1', 'P4', 'P6');
-select * from j where j.city='Rome';
-select * from p where p.color='Red';
-select * from spj order by s#;
-select * from s;
 
 /* Problem 8  (b) */
-
-select s# from spj 
-where
-(select count(distinct spj.j#) from p,j 
-  where p.color='Red' and j.city='Rome'
-  and spj.p#=p.p# and spj.j#=j.j#) =
-(select count(*) from j where j.city='Rome');
-
-/* i think count(*) from j because j is primary key
-   count distinct from spj, p, j because table can have
-   multi j#'s */
 
 select s# from s
  where (select count(distinct j#) from spj where spj.s#=s.s#
